2883f181d5d21271db8ac5b7da6c22e4a5293a09
Alvaro Estevez <alvaro5estevez@gmail.com>
2024-01-31 22:54:37 +0000

    Add comments and refactor

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 // Creamos el tablero vacio 
-let board = [
+let board = [//tablero vacio
     ['-', '-', '-'],
     ['-', '-', '-'],
     ['-', '-', '-']
 ]
 
-const resButton = document.getElementsByTagName('button')[0]
-const cells = [...document.getElementsByTagName('td')]
-const gameStatus = document.querySelector('h3')
+const resButton = document.getElementsByTagName('button')[0]//traemos el boton de reseteo
+const cells = [...document.getElementsByTagName('td')]//traemos los td y lo convertimos en array con spread operator
+const gameStatus = document.querySelector('h3')//traemos el mensaje de estado
 
 let player = 'X'
-let moveNumber = 0
-let isOver = false
+let moveNumber = 0 //numero de movimientos
+let isOver = false //es in empate
 
-const isSelected = (row, col) => {
+function selectCell(e) {//comprobamos si la celda esta marcada, si no lo está la marcamos
+    let target = e.target
+    let row = target.className.slice(-1) -1
+    let col = target.parentNode.className.slice(-1) -1
+
+    if(!isSelected(row, col)) {//comprobamos que no este seleccionada
+        updateCanva(e,row, col)
+        if(checkWin()) {//si gana llamamos a playerWin
+            playerWin()
+        }
+        if(!checkWin() && moveNumber > 8) {//en caso de que nadie haya ganado y el tablero 
+            isOver = true//este lleno decimos que es un empate y llamamos a playerwin
+            playerWin()
+        }
+        changePlayer()//cambiamos de jugador
+    }
+}
+
+function updateCanva(e,row, col) {//actualizamos el tablero y el array que lo representa
+    board[row][col] = player
+    e.target.innerHTML = player
+    e.target.classList.add('selected')
+    moveNumber++//aumentamos el numero de movimientos para saber si ya se hicieron todos
+}
+
+function isSelected(row, col){//comprobamos que la celda esta vacia
     return board[row][col] !== '-' ? true : false
 }
 
-const changePlayer = () => {
+function changePlayer(){//cambiamos de jugador a menos que se haya acabado la partida
     player = (player === 'X' ? 'O' : 'X')
     if(moveNumber < 9 && !checkWin()) {
         gameStatus.innerHTML = 'Player ' + player + "'s turn"
     }
 }
 
-const checkWin = () => {
+function checkWin(){//comprobamos que se haya ganado
     if(board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] !== '-') {
         return true
     }else if(board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] !== '-') {
@@ -46,14 +71,14 @@ const checkWin = () => {
     }
 }
 
-const playerWin = () => {
-    if(isOver){
+function playerWin(){
+    if(isOver){//si hay empate ponemos todo en rojo
         gameStatus.innerHTML = 'Game Over! Its a tie!'
         cells.forEach(cell => {
             cell.classList.remove('selected')
             cell.classList.add('loose')
         })
-    }else{
+    }else{//si un jugador gana se pone en verde y aparece quien gano
         gameStatus.innerHTML = 'Player ' + player + ' wins!'
         cells.forEach(cell => {
             cell.classList.remove('selected')
@@ -62,7 +87,7 @@ const playerWin = () => {
     }
 }
 
-const resetGame = () => {
+function resetGame(){//reiniciamos la partida
     board = [
         ['-', '-', '-'],
         ['-', '-', '-'],
@@ -80,34 +105,11 @@ const resetGame = () => {
     })
 }
 
-const selectCell = (e) => {
-    let target = e.target
-    let row = target.className.slice(-1) -1
-    let col = target.parentNode.className.slice(-1) -1
-
-    if(!isSelected(row, col)) {
-        board[row][col] = player
-        e.target.innerHTML = player
-        e.target.classList.add('selected')
-        moveNumber++
-    }
-    if(checkWin()) {
-        playerWin()
-    }
-    if(!checkWin() && moveNumber > 8) {
-        isOver = true
-        playerWin()
-
-    }
-    console.log(player)
-    changePlayer()
-    console.log(player)
-}
 
 
 
-cells.forEach(button =>{
+cells.forEach(button =>{//escuchamos donde se ha clicado para marcarlo
     button.addEventListener('click',selectCell)
 })
 
-resButton.addEventListener('click',resetGame)
+resButton.addEventListener('click',resetGame)//boton de reinicio
